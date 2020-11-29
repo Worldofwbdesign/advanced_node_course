ebfd68ca2a5041d72e9e7006a6f7ae56b113ea0d
Roman Martynenko <rmartynenko@deloitte.com>
2020-11-29 21:38:13 +0200

    Added cache service

---
+++ services/cache.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+const redisUrl = 'redis://127.0.0.1:6379'
+const redisClient = redis.createClient(redisUrl)
+redisClient.hget = util.promisify(redisClient.hget)
+
+const exec = mongoose.Query.prototype.exec
+
+mongoose.Query.prototype.cached = function({ key = 'default' } = {}) {
+  this.withCache = true
+  console.info('this.withCache cached', this.withCache)
+  this.hashKey = key
+
+  return this
+}
+
+mongoose.Query.prototype.exec = async function() {
+  console.info('this.withCache', this.withCache)
+  if (!this.withCache) return exec.apply(this, arguments)
+
+  const key = JSON.stringify(Object.assign(
+    {},
+    this.getFilter(),
+    { collection: this.mongooseCollection.name }
+  ))
+
+  const cachedValue = await redisClient.hget(this.hashKey, key)
+
+  if (cachedValue) {
+    const doc = JSON.parse(cachedValue)
+    console.info('doc', doc)
+    return Array.isArray(doc)
+      ? doc.map(v => new this.model(v))
+      : new this.model(doc)
+  }
+
+  const result = await exec.apply(this.arguments)
+  redisClient.hset(this.hashKey, key, JSON.stringify(result))
+
+  return result
+}
